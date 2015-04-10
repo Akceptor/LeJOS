eb5fa43ac6507b9302ec47168866d47db09ca18c
Akceptor <akceptor.spam@gmail.com>
2015-04-10 20:11:04 +0300

    Add distance measurement support

--- EV3RemoteControl/src/org/akceptor/lejos/RCRobotLib.java
+++ EV3RemoteControl/src/org/akceptor/lejos/RCRobotLib.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 
 import lejos.hardware.Button;
+import lejos.hardware.Sound;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
@@ -11,6 +12,8 @@ import lejos.hardware.port.Port;
 import lejos.hardware.sensor.EV3IRSensor;
 import lejos.hardware.sensor.SensorModes;
 import lejos.robotics.RegulatedMotor;
+import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
 
 import org.apache.commons.lang3.ArrayUtils;
 
@@ -28,19 +31,29 @@ public class RCRobotLib {
 		// IR Remote
 		Port port = LocalEV3.get().getPort("S1");
 		SensorModes sensor = new EV3IRSensor(port);
+		SampleProvider provider = sensor.getMode("Distance");
+		float[] sample = new float[provider.sampleSize()];
+
 		System.out.println("Press any key to start");
 		Button.waitForAnyPress();
 		System.out.println("Press ESCAPE key to stop");
 		int keys = 0;
 		while (keys != 32 /* ESCAPE */) {
-			byte[] commands = { 0, 0, 0, 0 };
+			// Control
+			byte[] commands = { 0, 0, 0, 0, 0 };
 			// Get array of commands: one per channel
 			((EV3IRSensor) sensor).getRemoteCommands(commands, 0, 4);
+			provider.fetchSample(sample, 0);
+			if (sample[0] < 20) {
+				avoidObstacleDetected(m1, m2);
+			}
 			// Current command and channel
-			int command = maxValue(commands);
-			int multiplier = maxIndex(commands);
+			// Channel 0 is used for distance measurement now so can't be used
+			// for RC
+			int command = maxValue(Arrays.copyOfRange(commands, 1, 3));
+			int multiplier = maxIndex(Arrays.copyOfRange(commands, 1, 3));
 			// Speed depends on channel
-			// 200->400->600->800
+			// NONE->400->600->800
 			m1.setSpeed(200 * (1 + multiplier));
 			m2.setSpeed(200 * (1 + multiplier));
 			switch (command) {
@@ -62,43 +75,72 @@ public class RCRobotLib {
 				break;
 			case 5:
 				// both fwd
-				m2.startSynchronization();
-				m1.forward();
-				m2.forward();
-				m2.endSynchronization();
+				goForward(m1, m2);
 				break;
 			case 6:
 				// rotate left
-				m2.startSynchronization();
-				m1.backward();
-				m2.forward();
-				m2.endSynchronization();
+				goLeft(m1, m2);
 				break;
 			case 7:
 				// rotate right
-				m2.startSynchronization();
-				m1.forward();
-				m2.backward();
-				m2.endSynchronization();
+				goRight(m1, m2);
 				break;
 			case 8:
 				// both bwd
-				m2.startSynchronization();
-				m1.backward();
-				m2.backward();
-				m2.endSynchronization();
+				goBackWard(m1, m2);
 				break;
 			default:
 				// stop
-				m2.startSynchronization();
-				m1.stop();
-				m2.stop();
-				m2.endSynchronization();
+				goStop(m1, m2);
 			}
 			// Acquire keys
 			keys = Button.readButtons();
 		}
 
+	}
+
+	private static void avoidObstacleDetected(RegulatedMotor m1, RegulatedMotor m2) {
+		m1.setSpeed(500);
+		m2.setSpeed(500);
+		goBackWard(m1, m2);
+		Sound.playTone(1000, 1000);
+		Delay.msDelay(2000);
+		goStop(m1, m2);
+	}
+
+	private static void goForward(RegulatedMotor m1, RegulatedMotor m2) {
+		m2.startSynchronization();
+		m1.forward();
+		m2.forward();
+		m2.endSynchronization();
+	}
+
+	private static void goLeft(RegulatedMotor m1, RegulatedMotor m2) {
+		m2.startSynchronization();
+		m1.backward();
+		m2.forward();
+		m2.endSynchronization();
+	}
+
+	private static void goRight(RegulatedMotor m1, RegulatedMotor m2) {
+		m2.startSynchronization();
+		m1.forward();
+		m2.backward();
+		m2.endSynchronization();
+	}
+
+	private static void goStop(RegulatedMotor m1, RegulatedMotor m2) {
+		m2.startSynchronization();
+		m1.stop();
+		m2.stop();
+		m2.endSynchronization();
+	}
+
+	private static void goBackWard(RegulatedMotor m1, RegulatedMotor m2) {
+		m2.startSynchronization();
+		m1.backward();
+		m2.backward();
+		m2.endSynchronization();
 	}
 
 	/**
